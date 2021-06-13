7ee956b144f54cce3091880014733a39931ba009
Ujjen <ujjen263@mail.fresnostate.edu>
2021-06-12 17:08:42 -0700

    Intro screen design and integration. Static texts made dynamic

--- components/player.js
+++ components/player.js
@@ -6,6 +6,11 @@ class Player {
     this.shielded = false;
     this.spearFlag = false;
     this.stolenDagger = 2;
+    this.name = self;
+  }
+
+  setName(name) {
+    this.name = name;
   }
 
   autoPlayGame() {
@@ -43,7 +48,7 @@ class Player {
     } else {
       this.hit(5);
       this.animatePlayer("hit-action");
-      this.updateAction("You hit rival with sword.");
+      this.updateAction(this.name + " hit rival with sword.");
       this.showSlash();
       setTimeout(() => {
         this.endSlash();
@@ -117,7 +122,7 @@ class Player {
       this.hit(10);
       this.movePlayer(10);
       this.animatePlayer("hit-action");
-      this.updateAction("You moved closer and hit rival with a dagger.");
+      this.updateAction(this.name + " moved closer and hit rival with a dagger.");
       new Audio('assets/audio/sword_audio.mp3').play();
       setTimeout(() => {
         this.removeAnimationClass("hit-action");
@@ -134,7 +139,7 @@ class Player {
       this.setShielded(true);
       this.animatePlayer("shield-action");
       this.movePlayer(-15);
-      this.updateAction("You moved farther and shielded yourself.");
+      this.updateAction(this.name + " moved farther and shielded yourself.");
       new Audio('assets/audio/shield_audio.mp3').play();
       setTimeout(() => {
         this.removeAnimationClass("shield-action");
@@ -176,7 +181,7 @@ class Player {
     new_button.classList.remove("hidden-action");
     this.spearFlag = 2;
 
-    this.updateAction("You used blackmagic. Satan gave you a spear that burns when used for two turns.");
+    this.updateAction(this.name + " used blackmagic. Satan gave you a spear that burns when used for two turns.");
   }
 
   isShielded() {
@@ -257,7 +262,7 @@ class Player {
     var temp = player.children[2].children[1].value;
     player.children[2].children[1].value = player.children[2].children[3].value;
     player.children[2].children[3].value = temp;
-    this.updateAction("You used blackmagic. Your health and distance values exchanged.");
+    this.updateAction(this.name + " used blackmagic. Your health and distance values exchanged.");
   }
 
   randomizeMoves() {
@@ -266,7 +271,7 @@ class Player {
     } else if(this.rival == "player1") {
       player1.setRandomMoves();
     }
-    this.updateAction("You randomized opponents moves.");
+    this.updateAction(this.name + " randomized opponents moves.");
   }
 
   setRandomMoves() {
@@ -278,7 +283,7 @@ class Player {
     var player2 = document.getElementById(this.rival);
     player2.children[1].children[1].disabled = true;
     this.stolenDagger = 2;
-    this.updateAction("You used blackmagic. You stole rivals dagger for two turns.");
+    this.updateAction(this.name + " used blackmagic. You stole rivals dagger for two turns.");
   }
 
   fireSpearAttack() {
@@ -292,7 +297,7 @@ class Player {
       setTimeout(() => {
         this.endExplosion();
       }, 1000);
-      this.updateAction("You hit rival with fire spear but i burnt your hand.");
+      this.updateAction(this.name + " hit rival with fire spear but i burnt your hand.");
       new Audio('assets/audio/fire_audio.mp3').play();
       this.checkFireSpear();
       setTimeout(() => {
